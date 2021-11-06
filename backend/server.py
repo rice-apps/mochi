25173caef0abff18b8ea785f29f58990c22dc5db
Taha Hasan <tahahasanofficial@gmail.com>
2021-11-06 15:01:28 -0500

    Created 2 endpoints

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,45 @@
 from flask import Flask
+import psycopg2
+import json
+
 app = Flask(__name__)
 
-@app.route("/hello")
+def list_to_json(lst):
+    res = {}
+    res["users"] = []
+    for inner_list in lst:
+        res_dict = {}
+        res_dict["netid"], res_dict["major"], res_dict["name"] = inner_list
+        res["users"].append(res_dict)
+    return json.dumps(res)
+
+@app.route("/hello/")
 def hello_world():
     return "Hello, world!\n"
+
+@app.route("/users/")
+def get_users():
+    conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", port = "3142", password="edmondkirsch3142")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users;")
+    return list_to_json(cur.fetchall())
+
+@app.route("/users/<netid>/")
+def get_user(netid):
+    conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", port = "3142", password="")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users WHERE netid = '" + str(netid) + "';")
+    return list_to_json(cur.fetchall())
+
+@app.route("/users/<netid>/")
+def get_user(netid):
+    conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", port = "3142", password="")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users WHERE netid = '" + str(netid) + "';")
+    return list_to_json(cur.fetchall())
+
+@app.route("/create_user/<netid>/<name>/<major>")
+def create_user(netid, name, major):
+    conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", port = "3142", password="")
+    cur = conn.cursor()
+
--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      
+      <View style={styles.countContainer}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setCount(count + 1)}
+      >
+        <Text style = {styles.buttonText}>Click me</Text>
+      </TouchableOpacity>
+      <Text style={styles.heading}>You clicked {count} times.</Text>
+      </View>
+
+      <View style={styles.nameContainer}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setName}
+        value={name}
+        placeholder="Your Name"
+      />
+      <Text style={styles.redMessage}>{name ? "Hello " + name + "!" : ""}</Text>
+      </View>
+
     </View>
   );
 }
@@ -12,8 +40,50 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "yellow",
+    alignItems: "center", 
+    justifyContent: "center"
+  },
+
+  countContainer: {
+    marginBottom: 50, 
+    alignItems: "center"
+  },
+
+  nameContainer: {
+    alignItems: "center"
+  },
+
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+
+  redMessage: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "red",
+  },
+
+  button: {
+    alignItems: "center",
+    backgroundColor: "black",
+    padding: 10,
+    marginBottom: 10,
+    width: 100
+  },
+
+  buttonText: {
+    fontSize: 10,
+    fontWeight: "bold",
+    color: "white",
+  },
+
+  input: {
+    height: 40,
+    width: 300,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
