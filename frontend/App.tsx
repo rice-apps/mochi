c99902e54727d464431e2dae09579701cf34853d
Freda Zhang <xz83@rice.edu>
2022-01-15 15:00:08 -0600

    merged with changes in origin

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -5,15 +5,35 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import { Loading, Welcome } from './components'
 
+import EventCard from "./components/EventCard";
+
+import { globalStyles } from './global';
+
+import {
+  View, Text, StyleSheet
+} from "react-native";
+
 const Stack = createNativeStackNavigator()
 
 export default function App (): JSX.Element {
   return (
+    <View>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name='Welcome' component={Welcome} />
         <Stack.Screen name='Loading' component={Loading} />
       </Stack.Navigator>
     </NavigationContainer>
+
+    <View style={globalStyles.container}>
+      <Text >This Month</Text>
+      {/* style = {styles.title} */}
+      <EventCard>
+      </EventCard>
+      <EventCard>
+      </EventCard>
+      </View>
+
+    </View>
   )
 }
