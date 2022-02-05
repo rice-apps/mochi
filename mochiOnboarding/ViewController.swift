fd6f857b728e8acc62730435e35aa67f392ee1f8
kbonilla2 <bonillakatharine@gmail.com>
2022-02-05 12:33:48 -0600

    Initial Commit

---
+++ mochiOnboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mochiOnboarding
+//
+//  Created by Katharine Bonilla on 2/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
