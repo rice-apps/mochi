e6528b003f57c81f54c061269660a973f5809a5b
Taha Hasan <tahahasanofficial@gmail.com>
2022-02-14 17:54:37 -0600

    Fix get_events endpoint, add create_user endpoint

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,16 @@
+from playhouse.postgres_ext import *
+from peewee import *
+import datetime
 import json
 
 from flask import Flask
-
+from flask import request
 app = Flask(__name__)
 
-import datetime
 
-from peewee import *
-from playhouse.postgres_ext import *
+pg_db = PostgresqlExtDatabase("postgres", user="postgres",
+                              password="edmondkirsch3142", host="localhost", port=3142)
 
-pg_db = PostgresqlExtDatabase("postgres", user="postgres", password="", host = "localhost", port=3142)
 
 class BaseModel(Model):
     class Meta:
@@ -24,30 +25,63 @@ class User(BaseModel):
     interests = ArrayField(CharField)
     netid = CharField()
 
+    def get_users():
+        users = User.select()
+
+        user_list = []
+
+        # Add each user as a JSON to the output list
+        for user in users:
+            print(user.netid)
+            user_list.append(
+                json.dumps(
+                    {"name": user.name,
+                    "college": user.college,
+                    "year": user.year,
+                    "major": user.major,
+                    "interests": user.interests,
+                    "netid": user.netid
+                    }))
+        
+        return str(user_list)
+
+
 class Event(BaseModel):
     location = CharField()
     description = TextField()
     timestamp = DateTimeField(default=datetime.datetime.now)
 
-    def get_events(user_id):
+    def get_events(netid):
         events = (
             Event.select()
             .where(Event.timestamp > datetime.datetime.now())
             .join(UserEvent)
             .join(User)
-            .where(User.id == user_id)
+            .where(User.netid == netid)
             .order_by(Event.timestamp)
         )
 
+        event_list = []
+
+        # Add each event as a JSON to the output list
         for event in events:
             print(event.description)
+            event_list.append(json.dumps({"location": event.location,
+                                          "description": event.description,
+                                          "timestamp": str(event.timestamp)
+                                          }))
+
+        return str(event_list)
+
 
 class UserEvent(BaseModel):
     user = ForeignKeyField(User, backref="user_events")
     event = ForeignKeyField(Event, backref="event_users")
 
+
 @app.route("/create_tables/")
 def create_tables():
+    pg_db.drop_tables([User, Event, UserEvent], safe=True)
     pg_db.create_tables([User, Event, UserEvent], safe=True)
 
     user1 = User.create(
@@ -55,6 +89,7 @@ def create_tables():
         college="brown",
         year="2023",
         major="Computer Science",
+        netid="rj",
         interests=["Coding", "Rock Climbing"],
     )
     event1 = Event.create(
@@ -67,13 +102,36 @@ def create_tables():
     UserEvent.create(user=user1, event=event1)
 
     print("done\n")
-    return user1.id
+    return str(user1.id)
 
-@app.route("/<username>/events/")
-def upcoming_events(username):
-    user = get_object_or_404(User, User.name == username)
-    events = Event.get_events(user.id)
-    return object_list("upcoming_events.html", events, "upcoming_events", user=user)
+
+@app.route("/<netid>/events/")
+def upcoming_events(netid):
+    events = Event.get_events(netid)
+    return events
+
+@app.route("/users/")
+def get_users():
+    users = User.get_users()
+    return users
+
+@app.route("/create_user/", methods = ["POST"])
+def create_user():
+    try:
+        user_json = request.json
+        user_data = json.loads(user_json)
+        User.create(
+            name=user_data.name,
+            college=user_data.college,
+            year=user_data.year,
+            major=user_data.major,
+            netid=user_data.netid,
+            interests=user_data.interests,
+        )
+        return true
+    except:
+        return false
+    
 
 @app.route("/hello2/")
 def hello_world():
