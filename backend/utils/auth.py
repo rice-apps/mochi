a9973371de23aaa3829f25bf852b4d4c34e64665
spoken-angus <angusjelinek@gmail.com>
2022-02-26 12:39:48 -0600

    changed return type

--- backend/utils/auth.py
+++ backend/utils/auth.py
@@ -1,7 +1,17 @@
 from firebase import firebaseAdmin
+from rest_framework.exceptions import NotAuthenticated
 
 
 class FirebaseHelper(object):
+
+    class Auth:
+        def __init__(self, token=None, name=None, netid=None, idNumber=None, role=None, success=False):
+            self.token = token
+            self.name = name
+            self.netid = netid
+            self.idNumber = idNumber
+            self.role = role
+            self.success = success
 
     @staticmethod
     def get_token(token):
@@ -13,10 +23,8 @@ class FirebaseHelper(object):
             netid = decodedToken.firebase.sign_in_attributes['urn:oid:0.9.2342.19200300.100.1.1']
             idNumber = decodedToken.firebase.sign_in_attributes['urn:oid:1.3.6.1.4.1.134.1.1.1.1.19']
             role = decodedToken.firebase.sign_in_attributes['urn:oid:1.3.6.1.4.1.5923.1.1.1.5']
-            return fullName, netid, idNumber, role, token, true
+            return Auth(fullName, netid, idNumber, role, token, True)
 
         except ValueError:
-            raise NotAuthenticated(
-                "Invalid Token"
-            )
-            return false
+            print("Invalid Token")
+            return Auth()
