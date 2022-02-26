262858b3efbb3c900193dbcdd35bb36053a3f087
Freda Zhang <xz83@rice.edu>
2022-02-26 13:30:14 -0600

    created upcoming page

---
+++ UpcomingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UpcomingViewController.swift
+//  mochiOnboarding
+//
+//  Created by Freda Zhang on 2/26/22.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
