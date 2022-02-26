b76e69ca452947483a07eeb3d0c6a30b5caae96c
spoken-angus <angusjelinek@gmail.com>
2022-02-26 12:31:09 -0600

    changed return type

--- backend/utils/auth.py
+++ backend/utils/auth.py
@@ -13,7 +13,7 @@ class FirebaseHelper(object):
             netid = decodedToken.firebase.sign_in_attributes['urn:oid:0.9.2342.19200300.100.1.1']
             idNumber = decodedToken.firebase.sign_in_attributes['urn:oid:1.3.6.1.4.1.134.1.1.1.1.19']
             role = decodedToken.firebase.sign_in_attributes['urn:oid:1.3.6.1.4.1.5923.1.1.1.5']
-            return {fullName, netid, idNumber, role, token, true}
+            return fullName, netid, idNumber, role, token, true
 
         except ValueError:
             raise NotAuthenticated(
