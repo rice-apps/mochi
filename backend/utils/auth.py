c2857c76aec36c4d55f63be7f41cd84c8164f20b
spoken-angus <angusjelinek@gmail.com>
2022-03-05 14:11:43 -0600

    complete auth flow

--- backend/utils/auth.py
+++ backend/utils/auth.py
@@ -1,13 +1,10 @@
 import xmltodict
 import jwt
-
-config = {
-    CASValidateURL: 'https://idp.rice.edu/idp/profile/cas/serviceValidate'
-}
+import urllib
 
 
 def createToken(user):
-    token = jwt.encode({"id": user._id, "netid": user.netid},
+    token = jwt.encode({"netid": user[netid]},
                        "secret", algorithm="HS256")
     return token
 
@@ -15,8 +12,29 @@ def createToken(user):
 def verifyToken(token):
     try:
         payload = jwt.decode(token, "secret", algorithms=["HS256"])
-        id = payload.get('id')
-        return {"success": True, "id": id}
+        netid = payload['netid']
+        return {"success": True, "netid": netid}
     except:
         print("Error decoding jwt token")
         return {"success": False}
+
+
+def authenticateTicket(idToken):
+    try:
+        decoded_token = verifyToken(idToken)
+        netid = decoded_token['netid']
+        return {"success": True, "netid": netid}
+    except:
+        print("Error authenticating jwt token")
+        return {"success": False}
+
+
+def login_auth(ticket):
+    file = urllib.request.urlopen(
+        'https://idp.rice.edu/idp/profile/cas/serviceValidate?ticket={ticket}&service=http://localhost:3000')
+    data = file.read()
+    file.close()
+
+    data = xmltodict.parse(data)
+    netid = data['cas:serviceResponse']['cas:authenticationSuccess']['cas:user']
+    return netid
--- backend/utils/firebase.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-cred = credentials.ApplicationDefault()
-
-firebaseAdmin = firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://hedwig-279117.firebaseio.com'
-})
