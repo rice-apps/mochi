1835164f4ae70a3646db18193f2db96a4a1c2626
spoken-angus <angusjelinek@gmail.com>
2022-03-05 12:34:58 -0600

    fixed auth

--- backend/utils/auth.py
+++ backend/utils/auth.py
@@ -1,19 +1,22 @@
 import xmltodict
-import jwts
+import jwt
+
 config = {
     CASValidateURL: 'https://idp.rice.edu/idp/profile/cas/serviceValidate'
 }
 
 
 def createToken(user):
-    token = jwt.encode({id: user._id, netid: user.netid},
+    token = jwt.encode({"id": user._id, "netid": user.netid},
                        "secret", algorithm="HS256")
     return token
 
 
 def verifyToken(token):
     try:
-        decoded = jwt.decode(token, "secret", algorithms=["HS256"])
-        return {success: true, id: decoded[id]}
+        payload = jwt.decode(token, "secret", algorithms=["HS256"])
+        id = payload.get('id')
+        return {"success": True, "id": id}
     except:
         print("Error decoding jwt token")
+        return {"success": False}
