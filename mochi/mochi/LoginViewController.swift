e73f442afdb829e5c0a73e0d70b1f1fc4d682571
spoken-angus <angusjelinek@gmail.com>
2022-03-26 14:21:28 -0500

    rename login view

--- mochi/mochi/LoginViewController.swift
+++ mochi/mochi/LoginViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
     
     private let button: UIButton = {
         let button = UIButton()
