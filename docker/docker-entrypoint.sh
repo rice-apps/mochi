911467157419d79aacb31459e75a0a6a9f4f926b
Victor Song <vms2@rice.edu>
2022-04-09 15:02:47 -0500

    Stuff

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# activate our virtual environment here
+. /opt/pysetup/.venv/bin/activate
+
+# You can put other setup logic here
+
+# Evaluating passed command:
+exec "$@"
